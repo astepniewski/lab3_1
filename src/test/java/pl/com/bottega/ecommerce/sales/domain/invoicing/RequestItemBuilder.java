3096fba98a81402bad642dac0878e3de46a11a51
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2015-04-22 23:22:38 +0200

    Dodanie klasy RequestItemBuilder.java

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/RequestItemBuilder.java
@@ -0,0 +1,29 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class RequestItemBuilder {
+	private ProductData productData = new ProductDataBuilder().build();
+	private int quantity = 1;
+	private Money totalCost = new Money(0);
+
+	public RequestItemBuilder withProductData(ProductData data) {
+		this.productData = data;
+		return this;
+	}
+
+	public RequestItemBuilder withQuantity(int quantity) {
+		this.quantity = quantity;
+		return this;
+	}
+
+	public RequestItemBuilder witTotalCost(double cost) {
+		this.totalCost = new Money(cost);
+		return this;
+	}
+
+	public RequestItem build() {
+		return new RequestItem(productData, quantity, totalCost);
+	}
+}
